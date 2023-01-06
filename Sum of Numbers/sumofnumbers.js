bc4b415ec7eb86b62e69efd7dd55675c53433143
Prajwal77777 <96177425+Prajwal77777@users.noreply.github.com>
2023-01-06 11:02:55 +0545

    Create sumofnumbers.js

---
+++ Sum of Numbers/sumofnumbers.js
@@ -0,0 +1,31 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+// My Solution
+function getSum(a, b) {
+  let min = a < b ? a : b;
+  let max = a < b ? b : a;
+
+  let sum = 0;
+  for (var i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+  //Good luck!
+}
+
+//Others Solution
+const GetSum = (a, b) => {
+  let min = Math.min(a, b),
+    max = Math.max(a, b);
+  return ((max - min + 1) * (min + max)) / 2;
+};
